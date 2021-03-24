0049e7945374425f3df927a0cde1d08b1e5c62d9
sooyeon01 <80629395+sooyeon01@users.noreply.github.com>
2021-03-24 22:00:06 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {    
+      colors: {
+          yellow:{
+            0:'#FFC72A', 
+            10:'#FFDF86', 
+            20:'#EDB61B'
+          },
+          pink:{
+            0:'#FFA5A5', 
+            10:'#FFCFCF', 
+            20:'#EE7E7E'
+          },
+          green:{
+            0:'#2FA599', 
+            10:'#9AD8D1', 
+            20:'#1F857A'
+          },
+          purple:{
+            0:'#9FA9D8', 
+            10:'#CCD1E9', 
+            20:'#6C78AF'
+          },
+          indigo:{
+            0:'#3B4B6C'
+          },
+        }            
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
